ef31a504906f07560500630719fb699434d9f045
Luis Josafat Heredia Contreras <luisyosafat@gmail.com>
2016-11-21 13:24:47 -0600

    use app name file env

--- App/Driver/config/app.php
+++ App/Driver/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Melisa Driver',
+    'name' => env('APP_NAME', 'Melisa'),
     'keyapp'=>'driver',
 
     /*
